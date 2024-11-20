0722c58ed1ccc0e3ebb5e59ef46e02f1f4f05d07
Ruslan Bagautdinov <ruslan.bagautdinov2000@mail.ru>
2024-11-20 12:26:19 +0500

    comm

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -1,38 +1,35 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDao;
 import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public class UserServiceImpl implements UserService {
-    UserDaoJDBCImpl r = new UserDaoJDBCImpl();
+    private UserDao userDaoJDBC = new UserDaoJDBCImpl();
+
     public void createUsersTable() {
-    r.createUsersTable();
+        userDaoJDBC.createUsersTable();
     }
 
     public void dropUsersTable() {
-        r.dropUsersTable();
+        userDaoJDBC.dropUsersTable();
     }
 
     public void saveUser(String name, String lastName, byte age) {
-        r.saveUser(name, lastName, age);
-        System.out.printf("User с именем — %s добавлен в базу данных \n", name);
+        userDaoJDBC.saveUser(name, lastName, age);
     }
 
     public void removeUserById(long id) {
-        r.removeUserById(id);
+        userDaoJDBC.removeUserById(id);
     }
 
     public List<User> getAllUsers() {
-    List<User> list = r.getAllUsers();
-        for (User user : list) {
-            System.out.println(user);
-        }
-        return list;
+        return userDaoJDBC.getAllUsers();
     }
 
     public void cleanUsersTable() {
-        r.cleanUsersTable();
+        userDaoJDBC.cleanUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -7,15 +7,14 @@ public class Util {
     private final static String DB_URL = "jdbc:mysql://localhost:3306/sqldb";
     private final static String DB_USERNAME = "root";
     private final static String DB_PASSWORD = "root";
+    private static Connection connection;
 
-    public Connection getConnection() {
-
-        Connection connection;
+    public static Connection getConnection() {
         try {
-                connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
-            } catch (SQLException e) {
-                throw new RuntimeException("Не удалось подключиться к базе данных");
-            }
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+        } catch (SQLException e) {
+            throw new RuntimeException("Не удалось подключиться к базе данных");
+        }
 
         return connection;
     }
